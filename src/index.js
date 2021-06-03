85bf12a026c4a89dc834cd729aba3f697d3bc2fe
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-06-04 08:00:46 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -147,3 +147,33 @@
 // console.log(arr8);
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr1 = ["田中", "山田", "みむ"];
+// for (let index = 0; index < nameArr1.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr1[index]}です。`);
+// }
+
+// const nameArr2 = nameArr1.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr1.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num)=>{
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr1.map((name)=> {
+//   if (name === "みむ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
