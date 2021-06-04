4226f9fcfd2a804cc64cb71737c29e2d5e091fc2
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-06-05 07:50:29 +0900

    論理演算子の本当の意味を知ろう&&||

--- src/index.js
+++ src/index.js
@@ -194,3 +194,27 @@ const nameArr1 = ["田中", "山田", "みむ"];
 //   return num1 + num2 > 100 ? '１００を超えています！！' : '許容範囲内です。';
 // }
 // console.log(checkSum(50,40));
+
+/**
+ * 論理演算子の本当に意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か２はtrueになります。")
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も２もtrueになります。")
+// }
+
+// || は左側がfalseなら右側を返す
+// const num1 = null;
+// const fee1 = num1 || "金額設定です。";
+// console.log(fee1);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
